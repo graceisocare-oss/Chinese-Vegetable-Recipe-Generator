5e00ac0aba4c95b520850b607b34b596ac726aef
Grace Care <graceisocare@gmail.com>
2026-06-15 05:32:47 +0100

    Updated all files

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ function generaterecipe(event) {
 let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "7a4f5a11f027034d307t909d5doccba8";
 let context = 
-"You are well known for finding interesting recipes.  I need you to generate a Chinese, easy to follow Vegetable Recipe in basic HTML and separate each line with a <br />.  Make sure to follow the user instructions.  Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe and NOT at the beginning";
+"You are good for finding short vegetable recipes.  I need you to generate a short Chinese  Vegetable Recipe in basic HTML and separate each line with a <br />.  Follow the user instructions.  Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe NOT at the beginning";
 let prompt = `User instructions: Generate a Chinese Vegetable recipe for ${instructionsInput.value}`;
 let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-let poemElement = document.querySelector("#recipe");
-poemElement.classList.remove("hidden");
-poemElement.innerHTML = `<div class="generating">Generating a Chinese Vegetable recipe for ${instructionsInput.value}</div>`;
+let recipeElement = document.querySelector("#recipe");
+recipeElement.classList.remove("hidden");
+recipeElement.innerHTML = `<div class="generating">Generating a Chinese Vegetable recipe for ${instructionsInput.value}</div>`;
 
 console.log("Generating recipe");
 console.log(`Prompt: ${prompt}`);
