1367a3e41109260b6628a316e97b6a56ed57c6b6
Grace Care <graceisocare@gmail.com>
2026-06-19 21:15:29 +0100

    Madecorrections to all three files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 function displayRecipe(response) {
-
-    console.log("recipe generated");
    new Typewriter("#recipe", {
     strings: response.data.answer,
     autoStart: true,
@@ -14,17 +12,13 @@ function generaterecipe(event) {
 let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "7a4f5a11f027034d307t909d5doccba8";
 let context = 
-"You are good for finding short vegetable recipes.  I need you to generate a short Chinese  Vegetable Recipe in basic HTML and separate each line with a <br />.  Follow the user instructions.  Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe NOT at the beginning";
+"You are good at finding short vegetable recipes.  I need you to generate a short Chinese Vegetable Recipe in basic HTML and separate each line with a <br />.  Follow the user instructions.  Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe NOT at the beginning.";
 let prompt = `User instructions: Generate a Chinese Vegetable recipe for ${instructionsInput.value}`;
 let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 let recipeElement = document.querySelector("#chineseVegetableRecipes");
 recipeElement.classList.remove("hidden");
 recipeElement.innerHTML = `<div class="generating">Generating a Chinese Vegetable recipe for ${instructionsInput.value}</div>`;
-
-console.log("Generating recipe");
-console.log(`Prompt: ${prompt}`);
-console.log(`Context: ${context}`); 
 
 axios.get(apiURL).then(displayRecipe);
 }
